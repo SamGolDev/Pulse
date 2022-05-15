758c32769f522debfbc94b74acbca8c208e3a7f2
Sergey <galor2211@gmail.com>
2022-05-15 21:37:28 +0300

    add modal blocks and responsivee

--- src/js/script.js
+++ src/js/script.js
@@ -35,6 +35,29 @@ $(document).ready(function(){
       };
         toggleSlide('.catalog-item__link');
         toggleSlide('.catalog-item__back');
+
+
+
+        //Modal модальные окна скрипт
+         /* обращение к классу          юзер кликает и происходит функция */
+      $('[data-modal=consultation]').on('click', function() {
+          /* обращение к классу         появление класса (анимация jquery) */
+        $('.overlay, #consultation').fadeIn('200');
+      });
+      $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('200');
+      });
+     
+       /*  чтобы текст названия товара менялся в модалке */
+       /* обращение к кнопке, each-для каждой кнопке будет выполняться функция, i отвечает за номер элемента */
+      $('.button_mini').each(function(i)  {
+      /* this-та кнопку, на которую нажал будет выполняться функция   */ 
+        $(this).on('click', function()  {
+/* внутри модалки order есть класс descr, туда помещаем текст из класса внизу,eq i чтобы получить нужный текст,.text()-вытягиваем текст  */           
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('200');
+        });
+      });
   });
 
 
