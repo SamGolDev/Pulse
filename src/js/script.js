f4111f99cc7d713c59397d281d999975ccd98e02
Sergey <galor2211@gmail.com>
2022-05-24 13:12:09 +0300

    add animations with wow and animate.css

--- src/js/script.js
+++ src/js/script.js
@@ -137,10 +137,12 @@ $(document).ready(function(){
 		});
 
 		$("a[href^='#']").click(function() {
-			var _href = $(this).attr("href");
+			const _href = $(this).attr("href");
 			$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 			return false;
 		});
+
+		new WOW().init();           /* подключили библу WOW для анимаций */
 	});
 
 
